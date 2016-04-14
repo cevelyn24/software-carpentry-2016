421b11d230cbe96ca3e8c3ec88131aff01fadae1
cevelyn24 <cevelyn24@gmail.com>
2016-04-14 16:50:37 -0700

    end of first day save, conditionals

--- r_scripts/R_practice1.R
+++ r_scripts/R_practice1.R
@@ -277,3 +277,89 @@ for(file in filenames){
   analyze(file)
 }
 
+#####making choices######
+
+
+analyze <- function(filename) {
+  # Plots the average, min, and max inflammation over time.
+  # Input is character string of a csv file.
+  dat <- read.csv(file = filename, header = FALSE)
+  avg_day_inflammation <- apply(dat, 2, mean)
+  plot(avg_day_inflammation)
+  max_day_inflammation <- apply(dat, 2, max)
+  plot(max_day_inflammation)
+  min_day_inflammation <- apply(dat, 2, min)
+  plot(min_day_inflammation)
+}
+
+
+analyze_all <- function(pattern) {
+  # Runs the function analyze for each file in the current working directory
+  # that contains the given pattern.
+  filenames <- list.files(pattern = pattern, full.names = TRUE)
+  for (f in filenames) {
+    analyze(f)
+  }
+}
+
+#print graphs or plots as pdf
+pdf("inflammation-01.pdf")
+analyze_all("inflammation-01.csv")
+dev.off()pdf #need to turn it off
+
+
+
+#####Conditionals#####
+
+#if then clause
+
+num<- 888
+
+if(num>100){
+  print("greater")
+  }else{
+      print("not greater")
+    }
+print("done")
+
+
+####another example
+
+num<-999
+
+if(num>100){
+  print("number is greater than 100")
+}
+
+####another example
+
+sign<-function(num){
+  if (num>0){
+    return(1)
+      } else if(num==0){
+        return(0)
+      } else {
+        return(-1)
+      }
+}
+
+sign(-3)
+
+#can use other conditions <=, >=, != is NOT equal to
+
+###with a string of tests want to use "and" "or"
+
+if (1> 0 & -1 > 0){
+  print ("both parts are true")
+} else {
+  print("at least one part is not true")
+}
+
+
+##"or" statement represented by |. pipe is or
+
+if (1> 0 | -1 > 0){
+  print ("at least one part is true")
+} else {
+  print("at least one part is not true")
+}
